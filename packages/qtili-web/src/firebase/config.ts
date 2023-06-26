5f2d682cdebd772801c46d757805b677e0b54515
Sergey Dedkov <dsv.mail@yandex.ru>
2023-06-26 14:54:45 +0600

    web emulator connection config

--- packages/qtili-web/src/firebase/config.ts
+++ packages/qtili-web/src/firebase/config.ts
@@ -1,7 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore/lite";
+import {
+  getFirestore,
+  connectFirestoreEmulator,
+} from "firebase/firestore/lite";
 
 const firebaseConfig = JSON.parse(import.meta.env.VITE_FIREBASE_CONFIG_JSON);
 
 export const firebaseApp = initializeApp(firebaseConfig);
 export const db = getFirestore(firebaseApp);
+if (import.meta.env.VITE_FIRESTORE_EMULATOR_HOST) {
+  const [host, port] = import.meta.env.VITE_FIRESTORE_EMULATOR_HOST.split(":");
+  connectFirestoreEmulator(db, host, Number(port));
+}
